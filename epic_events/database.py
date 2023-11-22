18eac23c83217352f2a2dfe42babb0cefec8c4df
Weidenthal Sandra <spiritf0rest@hotmail.com>
2023-11-22 15:36:53 +0100

    fix: insert initial data

--- epic_events/database.py
+++ epic_events/database.py
@@ -19,24 +19,26 @@ def current_session():
 
 
 def insert_initial_data(session):
-    commercial = Role(name="commercial")
-    support = Role(name="support")
-    management = Role(name="management")
+    roles = session.scalars(select(Role)).all()
+    if not roles:
+        commercial = Role(name="commercial")
+        support = Role(name="support")
+        management = Role(name="management")
 
-    session.add_all([commercial, support, management])
-    session.commit()
+        session.add_all([commercial, support, management])
+        session.commit()
 
-    role = session.scalar(select(Role).where(Role.name == "management"))
-    load_dotenv()
-    first_user_password = getenv("FIRST_USER_PASSWORD")
-    first_user_email = getenv("FIRST_USER_EMAIL")
-    first_user_name = getenv("FIRST_USER_NAME")
-    first_user = User(name=first_user_name, email=first_user_email, role=role.id)
+        role = session.scalar(select(Role).where(Role.name == "management"))
+        load_dotenv()
+        first_user_password = getenv("FIRST_USER_PASSWORD")
+        first_user_email = getenv("FIRST_USER_EMAIL")
+        first_user_name = getenv("FIRST_USER_NAME")
+        first_user = User(name=first_user_name, email=first_user_email, role=role.id)
 
-    first_user.set_password(first_user_password)
+        first_user.set_password(first_user_password)
 
-    session.add(first_user)
-    session.commit()
+        session.add(first_user)
+        session.commit()
 
 
 if __name__ == "__main__":
